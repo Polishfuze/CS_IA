d5dab232e9968c9008c940fbbe2941702c660129
Polish  Fuze <33132667+Polishfuze@users.noreply.github.com>
2021-08-13 02:10:50 +0200

    DB stuff and sites

---
+++ flaskWebsite/databaseManagement.py
@@ -0,0 +1,27 @@
+import boto3
+from flask_bcrypt import Bcrypt
+
+def verifyPassword(username, password):
+    bcrypt = Bcrypt()
+    dynamodb = boto3.resource('dynamodb', region_name='eu-north-1')
+    table = dynamodb.Table('LoginTable')
+    response = table.get_item(Key={'username':username.lower()})
+    # print(response)
+    passwordHash = response['Item']['password']
+    isThePasswordCorrect = bcrypt.check_password_hash(passwordHash, password)
+    return isThePasswordCorrect
+
+def registerUser(username, password):
+    dynamodb = boto3.resource('dynamodb', region_name='eu-north-1')
+    bcrypt = Bcrypt()
+    table = dynamodb.Table('LoginTable')
+    data = {
+        'username':username.lower(),
+        'password': bcrypt.generate_password_hash(password).decode('utf-8'),
+        }
+    table.put_item(Item=data)
+
+if __name__ == '__main__':
+    pass
+    # registerUser('as', '123')
+    # print(getPasswordHash('M@gmail'))
--- flaskWebsite/flaskMain.py
+++ flaskWebsite/flaskMain.py
@@ -1,17 +1,32 @@
-from flask import Flask
-
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+dummyStudentData = [
+    {
+        'name':'Michal',
+        'time':'12:37',
+        'isinSchool':'False'
+    },
+    {
+        'name':'Chris',
+        'time':'12:38',
+        'isinSchool':'True'
+    }
+]
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return render_template('home.html', title='Name lmao')
 
 @app.route("/login")
 def login():
-    return "<h1>Login</h1>"
+    return render_template('login.html', title='Name - login')
 
+@app.route("/database")
+def databaseDisp():
+    return render_template('databaseDisp.html', students=dummyStudentData, title='Ur viewing the DB')
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
