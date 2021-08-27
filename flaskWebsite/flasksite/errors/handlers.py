81215ad5c93ec58bd755af2285cf089a3ce19b50
Polish  Fuze <33132667+Polishfuze@users.noreply.github.com>
2021-08-27 22:55:33 +0200

    reformatting

--- flaskWebsite/flasksite/errors/handlers.py
+++ flaskWebsite/flasksite/errors/handlers.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, session
 
 errors = Blueprint('errors', __name__)
 
+
 @errors.app_errorhandler(404)
 def error_404(error):
     if 'username' not in session:
